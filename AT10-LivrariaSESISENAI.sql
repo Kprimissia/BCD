35ee0ac769474474a7affb46ed6ba4f2447c0cb7
Kprimissia <159180445+Kprimissia@users.noreply.github.com>
2024-10-17 15:54:28 -0300

    Add files via upload

--- AT10-LivrariaSESISENAI.sql
+++ AT10-LivrariaSESISENAI.sql
@@ -1,77 +1,62 @@
-CREATE DATABASE Livraria;
+CREATE DATABASE LivrariaSESISENAI;
+-- Criado o Banco de Dados LivrariaSESISENAI
 
-use Livraria
+USE LivrariaSESISENAI;
 
--- criando tabela cliente
-CREATE TABLE CLIENTE (
- id_cliente INT PRIMARY KEY AUTO_INCREMENT,
- nome VARCHAR(45), 
- cpf VARCHAR(11), 
- rg VARCHAR(11), 
- email VARCHAR(45) 
- ); 
- -- criando tabela telefone
- CREATE TABLE CLIENTES_TELEFONE ( 
- id_telefone INT PRIMARY KEY AUTO_INCREMENT, 
- telefone VARCHAR(15), 
- CLIENTE_id_cliente INT, 
- FOREIGN KEY (CLIENTE_id_cliente) REFERENCES CLIENTE(id_cliente) 
- ); 
- -- criando tabela endereco de clientes
- CREATE TABLE CLIENTES_ENDERECO ( 
- id_endereco INT PRIMARY KEY AUTO_INCREMENT, 
- rua VARCHAR(45), 
- numero INT, 
- cidade VARCHAR(45), 
- estado VARCHAR(45), 
- CLIENTE_id_cliente INT, FOREIGN KEY (CLIENTE_id_cliente) 
- REFERENCES CLIENTE(id_cliente) 
- ); 
-  -- criando tabela pedido
- CREATE TABLE PEDIDO ( 
- id_pedido INT PRIMARY KEY AUTO_INCREMENT, 
- valor DECIMAL(10, 2), 
- data DATE, 
- CLIENTE_id_cliente INT, 
- FOREIGN KEY (CLIENTE_id_cliente) REFERENCES CLIENTE(id_cliente)
- );
- -- criando tabela editora
-  CREATE TABLE EDITORA ( 
- id_editora INT PRIMARY KEY AUTO_INCREMENT, 
- nome_editora VARCHAR(30), 
- nome_contato VARCHAR(30), 
- email VARCHAR(45), 
- contato VARCHAR(20)
- );
- 
- -- criando tabela livro
- CREATE TABLE LIVRO ( 
- id_livro INT PRIMARY KEY AUTO_INCREMENT, 
- titulo VARCHAR(45), 
- categoria VARCHAR(45), 
- isbn VARCHAR(13), 
- ano DATE, 
- valor DECIMAL(10, 2), 
- autor VARCHAR(45), 
- EDITORA_id_editora INT, 
- FOREIGN KEY (EDITORA_id_editora) REFERENCES EDITORA(id_editora)
- ); 
- 
--- criando tabela pedido
- 
- CREATE TABLE ITEM_PEDIDO ( 
- quantidade INT, 
- valor DECIMAL(10, 2), 
- PEDIDO_id_pedido INT, 
- LIVRO_id_livro INT, PRIMARY KEY (PEDIDO_id_pedido, LIVRO_id_livro), 
- FOREIGN KEY (PEDIDO_id_pedido) REFERENCES PEDIDO(id_pedido), 
- FOREIGN KEY (LIVRO_id_livro) REFERENCES LIVRO(id_livro)
- ); 
- 
--- criando tabela estoque 
- 
-CREATE TABLE ESTOQUE ( 
-quantidade INT, 
-LIVRO_id_livro INT, 
-PRIMARY KEY (LIVRO_id_livro), 
-FOREIGN KEY (LIVRO_id_livro) REFERENCES LIVRO(id_livro) );
+
+CREATE TABLE tb_cliente(
+id_cliente int PRIMARY KEY NOT NULL,
+nome varchar(30) NOT NULL,
+rg varchar(11) NOT NULL,
+cpf varchar(11) NOT NULL,
+email varchar(45) NOT NULL
+);
+
+
+CREATE TABLE tb_cliente_telefone(
+id_telefone int PRIMARY KEY NOT NULL,
+telefone varchar(15) NOT NULL
+);
+
+
+CREATE TABLE tb_cliente_endereco(
+id_endereco int PRIMARY KEY NOT NULL,
+rua varchar(45) NOT NULL,
+numero int NOT NULL,
+cidade varchar(45) NOT NULL,
+estado varchar(45) NOT NULL
+);
+
+CREATE TABLE tb_pedidos(
+id_pedido int PRIMARY KEY NOT NULL,
+valor decimal (10,2) NOT NULL,
+dat4 DATE NOT NULL,
+Cliente_id_cliente int NOT NULL
+);
+
+CREATE TABLE tb_item_pedido(
+quantidade int NOT NULL,
+valor decimal(10,2) NOT NULL
+);
+
+CREATE TABLE tb_livro(
+id_livro int PRIMARY KEY NOT NULL,
+titulo varchar(45) NOT NULL,
+categoria varchar(45) NOT NULL,
+isbn varchar(13) NOT NULL,
+ano year NOT NULL,
+valor DECIMAL(10,2) NOT NULL,
+autor varchar(30) NOT NULL
+);
+
+CREATE TABLE tb_estoque(
+quantidade int NOT NULL
+);
+
+CREATE TABLE tb_editora(
+id_editora int PRIMARY KEY NOT NULL,
+nome_editora varchar(45) NOT NULL,
+nome_contato varchar(45) NOT NULL,
+email varchar(45) NOT NULL,
+contato varchar(30) NOT NULL
+);
